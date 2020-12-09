e3dfcb15ab4a46236d33efd0e6b23166a90a481d
Dustin Bensing <dustin.bensing@googlemail.com>
2020-12-09 15:13:33 +0100

    recover code

---
+++ client/src/lib.rs
@@ -0,0 +1,85 @@
+#![allow(clippy::enum_variant_names, clippy::large_enum_variant)]
+
+use seed::{prelude::*, *};
+
+mod example_a;
+mod example_b;
+mod example_c;
+
+// ------ ------
+//     Init
+// ------ ------
+
+fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+    Model::default()
+}
+
+// ------ ------
+//     Model
+// ------ ------
+
+#[derive(Default)]
+struct Model {
+    example_a: example_a::Model,
+    example_b: example_b::Model,
+    example_c: example_c::Model,
+}
+
+// ------ ------
+//    Update
+// ------ ------
+
+enum Msg {
+    ExampleA(example_a::Msg),
+    ExampleB(example_b::Msg),
+    ExampleC(example_c::Msg),
+}
+
+fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::ExampleA(msg) => {
+            example_a::update(msg, &mut model.example_a, &mut orders.proxy(Msg::ExampleA));
+        }
+        Msg::ExampleB(msg) => {
+            example_b::update(msg, &mut model.example_b, &mut orders.proxy(Msg::ExampleB));
+        }
+        Msg::ExampleC(msg) => {
+            example_c::update(msg, &mut model.example_c, &mut orders.proxy(Msg::ExampleC));
+        }
+    }
+}
+
+// ------ ------
+//     View
+// ------ ------
+
+fn view(model: &Model) -> impl IntoNodes<Msg> {
+    div![
+        style! {
+            St::FontFamily => "sans-serif";
+            St::MaxWidth => px(460);
+            St::Margin => "auto";
+        },
+        h1!["Rust Full Stack Example"],
+        example_a::view(&model.example_a, view_intro).map_msg(Msg::ExampleA),
+        example_b::view(&model.example_b, view_intro).map_msg(Msg::ExampleB),
+        example_c::view(&model.example_c, view_intro).map_msg(Msg::ExampleC),
+    ]
+}
+
+fn view_intro<Ms>(title: &str, description: &str) -> Vec<Node<Ms>> {
+    vec![
+        hr![],
+        h2![title],
+        div![style! {St::MarginBottom => px(15)}, description],
+    ]
+}
+
+// ------ ------
+//     Start
+// ------ ------
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    App::start("app", init, update, view);
+}
