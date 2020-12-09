f05673e416f23bb85364f97b34ce92b47fa2e4aa
Dustin Bensing <dustin.bensing@googlemail.com>
2020-12-09 15:38:04 +0100

    remove unecessary parts from server source

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,6 @@
 use actix::prelude::*;
 use actix_files::{Files, NamedFile};
-use actix_multipart::Multipart;
 use actix_web::{get, post, web, App, HttpServer, Result};
-use futures::stream::StreamExt;
-use std::fmt::Write;
 use std::time;
 
 mod count_actor;
@@ -32,34 +29,6 @@ async fn delayed_response(delay: web::Path<u64>) -> String {
     format!("Delay was set to {}ms.", delay)
 }
 
-#[post("form")]
-async fn form(mut form: Multipart) -> String {
-    let mut name_text_pairs: Vec<(String, String)> = Vec::new();
-    while let Some(Ok(mut field)) = form.next().await {
-        let field_name = field
-            .content_disposition()
-            .and_then(|cd| cd.get_name().map(ToString::to_string))
-            .expect("Can't get field name!");
-
-        let mut field_bytes: Vec<u8> = Vec::new();
-        while let Some(Ok(bytes)) = field.next().await {
-            for byte in bytes {
-                field_bytes.push(byte)
-            }
-        }
-
-        let field_text = String::from_utf8_lossy(&field_bytes).into_owned();
-        name_text_pairs.push((field_name, field_text));
-    }
-
-    let mut output = String::new();
-    for (name, text) in name_text_pairs {
-        writeln!(&mut output, "{}: {}", name, text).unwrap();
-        writeln!(&mut output, "___________________").unwrap();
-    }
-    output
-}
-
 async fn index() -> Result<NamedFile> {
     Ok(NamedFile::open("./client/index.html")?)
 }
@@ -80,7 +49,6 @@ async fn main() -> std::io::Result<()> {
                 web::scope("/api/")
                     .service(send_message)
                     .service(delayed_response)
-                    .service(form)
                     .default_service(web::route().to(web::HttpResponse::NotFound)),
             )
             .service(Files::new("/pkg", "./client/pkg"))
